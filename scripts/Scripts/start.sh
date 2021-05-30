8e4d74c2ed14ef6c3a783e57a2a76f9bb1d05995
Alexandre LAM <alexandrelam@outlook.com>
2021-05-30 23:09:51 +0200

    updated script and added battery status

--- scripts/Scripts/start.sh
+++ scripts/Scripts/start.sh
@@ -4,3 +4,7 @@ setxkbmap -layout fr,us
 setxkbmap -option 'grp:win_space_toggle'
 
 xinput set-prop "Logitech G300s Optical Gaming Mouse" "libinput Accel Speed" -0.7
+xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled Default" 1
+xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Natural Scrolling Enabled Default" 1
