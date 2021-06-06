e3010326f70ef0f61d72290f1a65c1b0b022ace2
Alexandre LAM <alexandrelam@outlook.com>
2021-06-06 11:33:42 +0200

    arch install

--- scripts/Scripts/arch-install.sh
+++ scripts/Scripts/arch-install.sh
@@ -1,1 +1,1 @@
-sudo pacman -Syu --noconfirm neovim firefox pulseaudio pavucontrol neofetch htop lightdm lightdm-gtk-greeter xorg xfce4 xfce4-goodies discord i3 dmenu stow
+sudo pacman -Syu --noconfirm neovim firefox pulseaudio pavucontrol neofetch htop lightdm lightdm-gtk-greeter xorg gnome discord i3 dmenu stow networkmanager dhclient dhcpcd
