783b7660ecff22c91702373df2173e4083266bb0
Alexandre Lam <alexandrelam@outlook.com>
2021-11-21 11:45:18 +0100

    added start to i3

--- scripts/Scripts/start.sh
+++ scripts/Scripts/start.sh
@@ -3,7 +3,7 @@
 setxkbmap -layout fr,us
 setxkbmap -option 'grp:win_space_toggle'
 
-xinput set-prop "Logitech G300s Optical Gaming Mouse" "libinput Accel Speed" -0.65
+xinput set-prop "Logitech G300s Optical Gaming Mouse" "libinput Accel Speed" -0.5
 xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled Default" 1
 xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Natural Scrolling Enabled" 1
