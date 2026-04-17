95fff20e6d9317eaafd409c402fc8e2a19442fef
Alexandre LAM <alexandrelam@outlook.com>
2026-04-17 23:21:38 +0200

    suspend command

--- bash/linux/alias.zsh
+++ bash/linux/alias.zsh
@@ -2,6 +2,7 @@ alias homeserver="ssh alex@95.217.210.24"
 alias lg=lazygit
 alias capture='grim -g "$(slurp)" - | swappy -f -'
 alias sd='shutdown now'
+alias sp='systemctl suspend'
 alias update='sudo timeshift --check && yay -Syu --noconfirm'
 
 alacritty-theme() {
